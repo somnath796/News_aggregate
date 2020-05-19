c716489bec51deb50663716a9429635c3ee3174d
Somnath Chatterjee <somnathchatterjee796@gmail.com>
2020-05-19 23:57:11 +0530

    Adding changes

--- project_1_news_aggregator/settings.py
+++ project_1_news_aggregator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'is530b97ng2nk&nr7y)yjdc+srgp4af7uoafbzc_ml75eq8_7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
