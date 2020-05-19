249f2df82317bb70b784c68a2e2f7606fe7929c7
Somnath Chatterjee <somnathchatterjee796@gmail.com>
2020-05-20 01:34:29 +0530

    news/views.py updated

--- news/views.py
+++ news/views.py
@@ -21,43 +21,44 @@ def scrape(request):
 	session.headers ={"User-Agent":"Googlebot/2.1 (+http://www.google.com/bot.html)"}
 	url = "https://www.indiatoday.in/world"
 
-	for page in range(1,5):
-			url+='?page=%d'%page
+	#for page in range(1,5):
+			#url+='?page=%d'%page
 
-			content = session.get(url, verify=False).content
-			soup = BSoup(content,"html.parser")
-		    #News = soup.find_all('div',{"class": "view-content"})
+	content = session.get(url, verify=False).content
+	soup = BSoup(content,"html.parser")
+    #News = soup.find_all('div',{"class": "view-content"})
 
-		    #for article in News:
-			
-			News = soup.find_all('div',{"class": "catagory-listing"})
+    #for article in News:
+	
+	News = soup.find_all('div',{"class": "catagory-listing"})
 
-			for article in News:
-			
-				image_url = article.find('div',{"class": "pic"}).img['src']
-				title=  article.find('div',{"class": "detail"}).h2.a.contents[0]
-				link = str(url[:-6]+article.find('div',{"class": "detail"}).h2.a['href'])
-				try:
-					description = str(article.find('div',{"class": "detail"}).p.text)
-				except:
-					description = str(article.find('div',{"class": "detail"}).p)
+	for article in News:
+	
+		image_url = article.find('div',{"class": "pic"}).img['src']
+		title=  article.find('div',{"class": "detail"}).h2.a.contents[0]
+		link = str(url[:-6]+article.find('div',{"class": "detail"}).h2.a['href'])
+		try:
+			description = str(article.find('div',{"class": "detail"}).p.text)
+		except:
+			description = str(article.find('div',{"class": "detail"}).p)
 
 
-				new_headline = Headline()
-				new_headline.title = title
-				new_headline.url = link
-				new_headline.image = image_url
-				new_headline.description= description
+		new_headline = Headline()
+		new_headline.title = title
+		new_headline.url = link
+		new_headline.image = image_url
+		new_headline.description= description
 
-				try:
-					new_headline.save()
-				
-				except IntegrityError as e: 
-					if 'unique constraint' in e.args:
-						continue 
+		try:
+			new_headline.save()
+		
+		except IntegrityError as e: 
+			if 'unique constraint' in e.args:
+				continue 
 
 
 	return redirect("../")
     
     
+
 
